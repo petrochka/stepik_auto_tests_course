59909015b889bebb820eea341f62cff40a4c282a
Lina <linavasko11@mail.ru>
2022-06-07 16:03:04 +0300

    решения задачи по теме

---  составные сообщения об ошибках и поиск подстроки/1.py
+++  составные сообщения об ошибках и поиск подстроки/1.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 def test_input_text(expected_result, actual_result):
     assert expected_result == actual_result,f"expected {expected_result}, got {actual_result}"
