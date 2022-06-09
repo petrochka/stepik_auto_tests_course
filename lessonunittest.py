377cb220a48a146949111c41d8e769cf6ef0896d
Lina <linavasko11@mail.ru>
2022-06-09 12:16:14 +0300

    решения задачи по теме

--- lessonunittest.py
+++ lessonunittest.py
@@ -1,29 +1,45 @@
-from selenium import webdriver
-import time
 import unittest
 
-def link_t(link):
+
+from selenium import webdriver
+import time
+
+try:
+    link= "http://suninjuly.github.io/registration1.html"
+
     browser = webdriver.Chrome()
     browser.get(link)
 
-    browser.find_element_by_xpath(".//*[@placeholder = 'Введите имя']").send_keys("Иван")
-    browser.find_element_by_xpath(".//*[@placeholder = 'Введите фамилию']").send_keys("Иванов")
-    browser.find_element_by_xpath(".//*[@placeholder = 'Введите Email']").send_keys("drrr@mail.ru")
-    browser.find_element_by_css_selector("button.btn").click()
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_xpath('//div[1]/div[1]/input')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector('div.first_block > div.form-group.second_class > input')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name('third')
+    input3.send_keys("348@mail.ru")
+    # Отправляем заполненную форму
+    button = browser.find_element_by_class_name('btn')
+    button.click()
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
 
-    time.sleep(1)
-    return browser.find_element_by_tag_name("h1").text
+
+    class Test1(unittest.TestCase):
+        def test_2(self):
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Should be 'Congratulations! You have successfully registered!'")
+    # def test_2(self):
+    #   self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
 
-class TestReg(unittest.TestCase):
-    def test_reg1(self):
-        self.assertEqual(link_t("http://suninjuly.github.io/registration1.html"),
-                         "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
+    if __name__ == "__main__":
+        unittest.main()
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
 
 
-    def test_reg2(self):
-        self.assertEqual(link_t("http://suninjuly.github.io/registration2.html"),
-                         "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
-
-if __name__ == "__main__":
-    unittest.main()
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
