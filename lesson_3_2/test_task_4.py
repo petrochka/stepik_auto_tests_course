d5c66c3ff22d72eefcfb3a603fe7bd5b69a7824a
Lina <linavasko11@mail.ru>
2022-06-30 22:14:44 +0300

    решения задачи по теме

--- lesson_3_2/test_task_4.py
+++ lesson_3_2/test_task_4.py
@@ -3,6 +3,8 @@ import unittest
 from selenium import webdriver
 import time
 
+from selenium.webdriver.common.by import By
+
 try:
     link= "http://suninjuly.github.io/registration1.html"
 
@@ -10,17 +12,17 @@ try:
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element_by_xpath('//div[1]/div[1]/input')
+    input1 = browser.find_element(By.XPATH, '//div[1]/div[1]/input')
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_css_selector('div.first_block > div.form-group.second_class > input')
+    input2 = browser.find_element(By.CSS_SELECTOR, 'div.first_block > div.form-group.second_class > input')
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name('third')
+    input3 = browser.find_element(By.CLASS_NAME, 'third')
     input3.send_keys("348@mail.ru")
     # Отправляем заполненную форму
-    button = browser.find_element_by_class_name('btn')
+    button = browser.find_element(By.CLASS_NAME, 'btn')
     button.click()
     # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
 
