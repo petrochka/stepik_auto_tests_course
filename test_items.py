11efacb5a3714a2e53f19ad7f45b0d5e8f98455e
petrochka <106708877+petrochka@users.noreply.github.com>
2022-07-11 12:17:36 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_button_add_to_basket_exists(browser):
     browser.get(link)
-    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    button = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     assert button, "Button not found"
